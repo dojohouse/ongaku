383dbcd056c45afc9d3890867b2e16f8a056552e
xTEddie <edwardtranv1@gmail.com>
2021-03-10 02:56:00 -0800

    Update create tag endpoint to POST call

--- server/routes/tagRoutes.js
+++ server/routes/tagRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const tagController = require('../controllers/tagController');
 
 router.get('/tags', tagController.getTags);
-router.get('/tag/create', tagController.createTag);
+router.post('/tag/create', tagController.createTag);
 router.get('/tag/:id', tagController.getTag);
 
 module.exports = router;
