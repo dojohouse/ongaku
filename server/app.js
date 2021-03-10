32f17edc4fecc203bce2df313f1b6b589ff6699d
xTEddie <edwardtranv1@gmail.com>
2021-03-10 00:15:15 -0800

    Use tag routes

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const logger = require('morgan');
-const fetch = require('node-fetch');
+const tagRoutes = require('./routes/tagRoutes');
 const spotifyRoutes = require('./routes/spotifyRoutes');
 const qrRoutes = require('./routes/qrRoutes');
 
@@ -12,6 +12,7 @@ app.get('/', (req, res) => {
   res.send(':)');
 });
 
+app.use('/api', tagRoutes);
 app.use('/api/spotify', spotifyRoutes);
 app.use('/api/qr', qrRoutes);
 
