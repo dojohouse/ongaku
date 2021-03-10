79ad2812b0a2a4fb1a0c6c4b5e783c6219c1c04b
xTEddie <edwardtranv1@gmail.com>
2021-03-10 00:53:58 -0800

    Add ability to create new tags

--- server/utils/createConnection.js
+++ server/utils/createConnection.js
@@ -1,8 +1,25 @@
 const fs = require('fs');
+const uuid = require('uuid');
+const uuidv4 = uuid.v4;
 
 const TAGS_COLLECTION = './data/tags.json';
 
 class TagRepository {
+  async createTag(properties) {
+    const tag = {};
+    tag.tag_id = uuidv4();
+    tag.music_id = '';
+    tag.platform = '';
+
+    if (!properties) {
+      return tag;
+    }
+
+    tag.music_id = properties.music_id || '';
+    tag.platform = properties.platform || '';
+    return tag;
+  }
+
   async find() {
     const response = await fs.promises.readFile(TAGS_COLLECTION);
     return JSON.parse(response.toString())
@@ -11,6 +28,13 @@ class TagRepository {
   async findById(id) {
     const tags = await this.find();
     return tags.filter((tag) => tag.tag_id === id);
+  }
+
+  async save(tag) {
+    const response = await fs.promises.readFile(TAGS_COLLECTION);
+    const tags = JSON.parse(response.toString());
+    tags.push(tag);
+    await fs.promises.writeFile(TAGS_COLLECTION, JSON.stringify(tags, null, 2));
   }
 }
 
