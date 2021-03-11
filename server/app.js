929ba5a4b06b4b6e864a61c2f1a23b5b95e1e7a8
xTEddie <edwardtranv1@gmail.com>
2021-03-10 23:21:28 -0800

    Use music & auth routes

--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ const logger = require('morgan');
 const tagRoutes = require('./routes/tagRoutes');
 const spotifyRoutes = require('./routes/spotifyRoutes');
 const qrRoutes = require('./routes/qrRoutes');
+const musicRoutes = require('./routes/musicRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 app.use(express.json());
@@ -16,5 +18,7 @@ app.get('/', (req, res) => {
 app.use('/api', tagRoutes);
 app.use('/api/spotify', spotifyRoutes);
 app.use('/api/qr', qrRoutes);
+app.use('/api/music', musicRoutes);
+app.use('/api/auth', authRoutes);
 
 module.exports = app;
